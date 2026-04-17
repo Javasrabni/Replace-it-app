49545bd92f780c6828bdfce8256994de075d277a
Patheigenx <98625563+Javasrabni@users.noreply.github.com>
2026-04-18 00:23:14 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10880532
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-
-self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10880443
+    "zoneId": 10890317
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
